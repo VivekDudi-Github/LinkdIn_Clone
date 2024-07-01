f9f970d743cbae8338f0a6760d5340d940130df4
VivekDudi-Github <vivekdudi18@gmail.com>
2024-07-01 23:37:39 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Centeral_Bar , SideBar , Container_app, Right_bar } from "./index"
+
+
 import Post_Box from './components/POSTS/Post_Box'
 import Create_Post from './components/POSTS/Create_Post'
 import Post_Container from './components/POSTS/Post_Container'
+
+import Login_page from './components/Login_Logout/Login_page'
+
 
 
 function App() {
@@ -13,18 +18,21 @@ function App() {
   return (
     <>
     <div className='bg-black h-[100vh]'>
-    <SideBar/>
+    {/* <SideBar/>
     <Container_app>
           <Post_Container>
           <Create_Post/>
           <Post_Box/>  
           </Post_Container>
        
-          
+           */}
           
           {/* <Centeral_Bar/> */}
-          <Right_bar/>
-    </Container_app>
+          {/* <Right_bar/>
+    </Container_app> */}
+    <Login_page/>
+
+
     </div>
     </>
   )
---
+++ src/components/Login_Logout/Login_page.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+function Login_page() {
+  return (
+    <div className='text-white'>
+        <div className='flex flex-row w-[100%]'>
+        <div className='w-[50%] text-center m-auto flex justify-center items-center'>
+            <i className='fa-brands fa-x-twitter text-[300px] '></i>
+        </div>
+        <div className='w-[50%] mb-8  text-lefts pt-16 pl-20 '>
+            <div className='text-[64px] font-bold font-'>Happening Now</div>
+            <div className='text-[36px] mb-[40px] font-bold font-'>Join Now.</div>
+
+            <div className=' text-left'>
+                <div><button className=' bg-white rounded-full text-black font-sans font-bold p-2 my-2 w-72'><i className='fa-brands fa-google text-base m-2 '></i>Signup With Google</button></div>
+                <div><button className=' bg-white rounded-full text-black font-sans font-bold p-2 my-2 w-72'><i className='fa-brands fa-apple text-lg m-2'></i>Signup WIth Apple</button></div>
+
+                <div className='flex items-center '>
+                    <div className=' h-[1px] w-[134px] bg-gray-500'></div><div className='relative bottom-0.5 '>or</div>
+                    <div className=' h-[1px] w-[134px] bg-gray-500'></div>
+                </div>
+                <div><button className=' bg-blue-500 hover:bg-blue-600 rounded-full font-sans font-bold p-2 my-2 w-72'>Create Account</button></div>
+                <p className='w-72 text-[12px] mb-10'>By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.</p>
+                <h1>Already have a account ?</h1>
+                <div><button className='bg-black hover:bg-[#01326a59] border-[1px] mt-4 rounded-full font-sans font-bold p-2 my-2 w-72'>Sign In</button></div>
+            </div>    
+        </div>
+
+        </div>
+
+
+        {/*footer*/}
+        <div className='text-[.75em] text-gray-500 text-center bg-black'>
+        About
+Download the X app
+Help Center
+Terms of Service
+Privacy Policy
+Cookie Policy
+Accessibility
+Ads info
+Blog
+Careers
+Brand Resources
+Advertising
+Marketing
+X for Business
+Developers
+Directory
+Settings
+© 2024 X Corp.
+        </div>
+    </div>
+  )
+}
+
+export default Login_page
