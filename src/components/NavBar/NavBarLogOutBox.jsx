0246eb9a58d0a572cf7f5a083714e62adc4a6ed5
VivekDudi-Github <vivekdudi18@gmail.com>
2024-08-20 21:34:34 +0530

    signout func added

--- src/components/NavBar/NavBarLogOutBox.jsx
+++ src/components/NavBar/NavBarLogOutBox.jsx
@@ -1,3 +1,4 @@
+import { signOut } from "firebase/auth"
 
 function NavBarLogOutBox() {
   return (
@@ -5,8 +6,9 @@ function NavBarLogOutBox() {
     {/* <div className=' fixed h-full w-full text-white z-0'>
         NavBarLogOutBox
     </div> */}
-    <div className=" w-[324px] border-2 border-white z-50 fixed bottom-20 left-20 rounded-2xl py-3 bg-black ">
-        <div className="hover:bg-[#181717] duration-200 p-1 ">
+    <div className=" w-[324px] shadow-[0,4px,8px,0,rgba(0,0,0,0.2)] z-50 fixed bottom-20 left-20 rounded-2xl py-3 bg-black ">
+        <div className="hover:bg-[#181717] duration-200 p-1 "
+             onClick={signOut}>
             LogOut @NPM_run_Dev 
         </div>
     </div>
