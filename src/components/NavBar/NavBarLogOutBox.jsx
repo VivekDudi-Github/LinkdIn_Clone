5ab7e70d593ec1b62640f603d26edf1c8cb1034c
VivekDudi-Github <vivekdudi18@gmail.com>
2024-08-21 21:26:01 +0530

    updated

--- src/components/NavBar/NavBarLogOutBox.jsx
+++ src/components/NavBar/NavBarLogOutBox.jsx
@@ -7,7 +7,7 @@ function NavBarLogOutBox() {
         NavBarLogOutBox
     </div> */}
     <div className=" w-[324px] shadow-[0,4px,8px,0,rgba(0,0,0,0.2)] z-50 fixed bottom-20 left-20 rounded-2xl py-3 bg-black ">
-        <div className="hover:bg-[#181717] duration-200 p-1 "
+        <div className="hover:bg-[#181717] duration-200 p-1 rounded-xl "
              onClick={signOut}>
             LogOut @NPM_run_Dev 
         </div>
