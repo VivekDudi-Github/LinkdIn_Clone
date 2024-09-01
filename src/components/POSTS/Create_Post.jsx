eb1baab92d24984873203c2d1cad1fe75dcf12ba
VivekDudi-Github <vivekdudi18@gmail.com>
2024-09-01 21:43:21 +0530

    updated

--- src/components/POSTS/Create_Post.jsx
+++ src/components/POSTS/Create_Post.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import "./Post_Box.css"
 import Image from "../../assets/2a.jpg"
+import { useSelector } from 'react-redux'
 
 function Create_Post() {
-  return (
+    const userImage = useSelector(state => state.UserSlice?.userData?.mainImage)
+
+    return (
     <>
     <div className='flex  border-y-[0.5px] border-gray-600 pt-2 text-white  max-w-[800px]'>
-        <div className=' w-16 p-2 pt-1 pb-3  '>
-            <img className=' object-cover rounded-full w-[40px] h-[40px] 'src={Image} alt='Profile-Photo'/>
+        <div className=' w-16 p-2 pt-1 pb-3 '>
+            <img className=' object-cover rounded-full w-[40px] h-[40px] 'src={userImage} alt='Profile-Photo'/>
         </div>
 
         <div className='w-full'>
             <div className='text-white pt-3 '>
-                <textarea className='bg-black w-full focus:outline-none'
+                <input className='bg-black w-full mb-2 focus:outline-none'
                 placeholder='What is happening?!'/>
             </div>
             <div className='border-y border-gray-500 flex justify-between py-2 text-blue-500 '>
