ea601c15dee168b2ee1681e574071ee41a31a964
VivekDudi-Github <vivekdudi18@gmail.com>
2024-10-19 08:20:07 +0530

    minor bugs fixes and layout development

--- src/components/Containers/InPagebox_layout.jsx
+++ src/components/Containers/InPagebox_layout.jsx
@@ -4,7 +4,7 @@ function InPagebox_layout({children}) {
     <>
       
         <div className=' w-full h-full  z-50  fixed left-0 top-0   bg-[#104c9a4f] text-white flex justify-center items-baseline pt-16  '>
-            <span className='bg-black rounded-3xl  max-h-[80%]   w-full sm:w-2/5 sm:min-w-[500.99px]   relative   duration-200 '>
+            <span className='bg-black rounded-3xl  max-h-[80%]   w-full sm:w-2/5 sm:min-w-[500.99px]   relative   duration-200  '>
                 {children}
             </span>
         </div>
--- src/components/POSTS/Create_Post_Page.jsx
+++ src/components/POSTS/Create_Post_Page.jsx
@@ -18,8 +18,9 @@ function Create_Post_Page() {
   const isBoxOpen_state = useSelector(state => state.comp?.isCreateBox)
   const [isBoxOpen , setIsBoxOpen] = useState(true) ;
 
+  const [IsLocation , setIslocation] = useState(false)
 
-  
+   
   const [imagesForUpload , setimagesForUpload] = useState([])
 
   useEffect(() => {
@@ -38,8 +39,12 @@ function Create_Post_Page() {
         src={profileImage} alt='profile image'/>
       <TextareaAutosize className=' w-[90%]  h-10  right-0  p-2  bg-[#181818a5] outline  outline-gray-500 outline-1 focus:outline-gray-50  focus:outline-1 rounded-lg duration-200 ' placeholder='What is happening?!'
       maxRows={6}/>
-
     </span>
+    
+    <input className={` w-[40%]  h-10 ml-11 m-2  p-2  bg-[#181818a5] outline  outline-gray-500 outline-1 focus:outline-gray-50  focus:outline-1 rounded-lg duration-200 
+                        ${IsLocation ? "" : "hidden"}
+             `} placeholder='Location'/>
+    
   </div>)
 
 
@@ -57,8 +62,8 @@ function Create_Post_Page() {
     <>
      {isBoxOpen && 
       <InPagebox_layout>
-          
-    <div className=' relative h-full overflow-auto duration-200 pb-28'>
+        
+    <div className=' relative h-96 duration-200 pb-28'>
 
   {/* head */}
       <div className=' p-2 flex justify-between duration-200'>
@@ -75,41 +80,43 @@ function Create_Post_Page() {
 
 
   {/* input boxes */}
+      <div className='overflow-y-scroll max-h-[100%]'>
 
-      {input_box}
+        {input_box}
 
-      <div className='flex px-2 '>
-        <div className=' w-10 p-[1px]'>
-          <div className=' bg-gray-800   w-[1px] h-full mx-auto   rounded-full  '></div>
-        </div>
+        <div className=' flex px-2 '>
+          <div className=' w-10 p-[1px]'>
+            <div className=' bg-gray-800   w-[1px] h-full mx-auto   rounded-full  '></div>
+          </div>
 
         <div className='  mx-auto  pt-3 h-auto w-[90%] flex overflow-x-auto duration-200  '>
-           
-            {imagesForUpload.length > 0  && 
-              imagesForUpload.map((image , index) => {
-              let imageurl = URL.createObjectURL(image) 
-                
-      return <div className={`p-1 h-[300px] rounded-3xl relative flex-shrink-0 ${imagesForUpload.length >1 ? ` w-1/2` : "w-full"} `} key={index}>
-              <img className= " h-full w-full rounded-3xl object-cover relative "
-                      src={imageurl}  
-                      alt="uploaded photo"
-              />
-                <button className='w-8 h-8 bg-[#5e5c5c6c] hover:bg-[#363535a3] rounded-full absolute top-2 left-2 duration-200'
-                  onClick={() => removeImagesForUpload(index)}>
+            
+              {imagesForUpload.length > 0  && 
+                imagesForUpload.map((image , index) => {
+                let imageurl = URL.createObjectURL(image) 
+                  
+                return <div className={`p-1 h-[300px] rounded-3xl relative flex-shrink-0 ${imagesForUpload.length >1 ? ` w-1/2` : "w-full"} `} key={index}>
+                        <img className= " h-full w-full rounded-3xl object-cover relative "
+                              src={imageurl}  
+                              alt="uploaded photo"
+                      />
+                        <button className='w-8 h-8 bg-[#5e5c5c6c] hover:bg-[#363535a3] rounded-full absolute top-2 left-2 duration-200'
+                          onClick={() => removeImagesForUpload(index)}>
 
-                <i className='fa-solid fa-close p-1 '/>
-                </button>
-                
-              </div>
-              })}
+                        <i className='fa-solid fa-close p-1 '/>
+                        </button>
+                        
+                      </div>
+                      })}
+          </div>
         </div>
       </div>
 
   {/* bottom */}
        
         <div className=' absolute w-full  bottom-1'> 
-            <div className='px-2 py-1 text-blue-500 bg-white inline-block rounded-lg'>
-                <i className="fa-solid fa-globe " />
+            <div className='px-2 py-1 h-9 text-blue-500 w-[34%] min-w-[160px] bg-white block rounded-lg'>
+                <i className="fa-solid fa-globe" />
                 &nbsp; Everone can reply
             </div>
             <div className='border-t border-gray-500 flex justify-between py-2 text-blue-500'>
@@ -118,7 +125,7 @@ function Create_Post_Page() {
                       <button className="fa-solid fa-bars-progress post_box_icons"></button>
                       <button className="fa-regular fa-face-smile post_box_icons"></button>
                       <button className="fa-solid fa-calendar-week post_box_icons"></button>
-                      <button className="fa-solid fa-location-dot post_box_icons"></button>
+                      <button className="fa-solid fa-location-dot post_box_icons" onClick={()=> setIslocation(!IsLocation)}></button>
                   </div>
                   <input className='hidden' ref={UploadPhotoRef} type="file" accept='image/png, image/jpg, image/jpeg, image/gif '
                           onChange={(e) => handleImagesForUpload(e.target.files)} />
